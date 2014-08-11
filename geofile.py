032b52ac717ce512f0fdfdd4d2032926d94520a0
manojrammurthy <manrox.drag@gmail.com>
2014-08-11 09:25:58 -0400

    getting lat lng

--- geofile.py
+++ geofile.py
@@ -2,7 +2,7 @@ from sys import argv
 import urllib
 import json
 
-script, filename = argv
+#script, filename = argv
 pincode = raw_input("enter pincode> ")
 country = raw_input("countryname > ")
 
@@ -13,6 +13,17 @@ jdata = urllib.urlopen(url1)
 # the output we get is json but we cannot query on this data 
 # so we have to load this data as json in a variable
 fp = json.load(jdata)
-with open(filename, 'a') as outfile:
-    json.dump(fp, outfile, ensure_ascii = False)
+result = fp['results'][0]['geometry']['location']
+#for rs in result:
+    #print rs['lat']
+print result['lat'] 
+status = fp['status']
+if status  == 'ZERO_RESULTS':
+    print "indicates that the reverse geocoding was successful but returned no results"
+elif status == 'INVALID_REQUEST':
+    print "country and postal code may be wrong "
+elif status == 'UNKNOWN_ERROR':
+    print "UNKNOWN_ERROR may be due to server error"
+#with open(filename, 'a') as outfile:
+ #   json.dump(fp, outfile, ensure_ascii = False)
 
--- gettingaddress.py
+++ gettingaddress.py
@@ -8,12 +8,17 @@ print "Enter  the country name "
 country = raw_input("> ")
 url = "https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:"
 url1 = url + pincode + "|country:" + country
-#url = "https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:%d" % pincode
 print  url1
+#while True:
 jdata = urllib.urlopen(url1)
 fp = json.load(jdata)
+  #  if
+print fp['status']
 #print fp["results"]
+result = fp['results'][0]['geometry']['location']
+lat , lng = result['lat'], result['lng']
 city = json.dumps([s['address_components'][2]['long_name'] for s in fp['results']], indent=2)
+print lat, lng 
 print city
 db = sqlite3.connect('/home/lenovo/workspace/GeoPython/db/mydb')
 print "opened databaase succesfully"
@@ -23,8 +28,9 @@ cursor.execute('''
    CREATE TABLE IF NOT EXISTS geoadd(city TEXT,
                        pincode INTE, country TEXT)
 ''')
-
-cursor.execute('''INSERT INTO geoadd(city,pincode,country) VALUES(?,?,?)''',(city,pincode,country))
+#cursor.execute('''ALTER TABLE geoadd add column lat float''')
+#cursor.execute('''ALTER TABLE geoadd add column lng float''')
+cursor.execute('''INSERT INTO geoadd(city,pincode,country,lat,lng) VALUES(?,?,?,?,?)''',(city,pincode,country,lat,lng))
 db.commit()
 cursor.execute('''select * from geoadd''')
 
